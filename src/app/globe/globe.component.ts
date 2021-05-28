0cadbf002b8ceaccbcff27490aab28dbe57905fc
J-Guenther <jannik.guentheg@gmail.com>
2021-05-28 16:17:52 +0200

    added textures and made earth

--- src/app/globe/globe.component.ts
+++ src/app/globe/globe.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import * as THREE from 'three';
+import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 @Component({
   selector: 'app-globe',
@@ -25,9 +27,50 @@ export class GlobeComponent implements OnInit {
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
     const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // scene.add(cube);
 
-    camera.position.z = 5;
+    camera.position.z = 1;
+
+    // scene.add(new THREE.AmbientLight(0x333333));
+
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 1, 1);
+    scene.add(light);
+
+    const loader = new THREE.TextureLoader();
+
+    const map = loader.load('assets/blenderguru/Albedo.jpg');
+    const bumpMap = loader.load('assets/blenderguru/Bump_small.jpg');
+    const specularMap = loader.load('assets/blenderguru/Ocean_Mask_inverted_small.png');
+    console.log({map});
+    console.log({specularMap});
+
+
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(0.5, 32, 32),
+      new THREE.MeshPhongMaterial({
+        map: map,
+        bumpMap: bumpMap,
+        bumpScale: 0.001,
+        specularMap: specularMap,
+        specular: new THREE.Color('grey'),
+        shininess: 10
+      })
+    );
+    scene.add(sphere);
+    //webgl2renderingcontext.uniform2fv: argument 2 could not be converted to any of: float32array
+
+    const cloudTexture = loader.load('assets/blenderguru/Clouds_small.png');
+    const clouds = new THREE.Mesh(
+      new THREE.SphereGeometry(0.503, 32, 32),
+      new THREE.MeshLambertMaterial({
+        alphaMap: cloudTexture,
+        opacity: 1,
+        transparent: true,
+      })
+    );
+    sphere.add(clouds);
+
 
     function resizeCanvasToDisplaySize() {
       const canvas = renderer.domElement;
@@ -46,15 +89,15 @@ export class GlobeComponent implements OnInit {
       }
     }
 
+    // const controls = new TrackballControls(camera, canvas);
+    const controls = new OrbitControls(camera, canvas);
+
     const animate = function () {
-
       resizeCanvasToDisplaySize();
-
+      controls.update();
+      sphere.rotation.y += 0.0005;
+      clouds.rotation.y += 0.0005;
       requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
       renderer.render(scene, camera);
     };
 
